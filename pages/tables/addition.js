49e10ab2831d7d0114ccf8299dfeaab810586653
Jeff Patzer <jpatzer@llnw.com>
2022-10-18 04:53:16 +0700

    Step1

--- pages/tables/addition.js
+++ pages/tables/addition.js
@@ -13,7 +13,7 @@ import {
 import Layout from "../layout";
 import Head from "next/head";
 
-function AdditionTable({ data }) {
+function AdditionTable() {
   return (
     <Layout>
       <Head>
@@ -35,7 +35,7 @@ function AdditionTable({ data }) {
             </Tr>
           </Thead>
           <Tbody>
-            {data.map((m, i) => {
+            {matrix.map((m, i) => {
               return (
                 <Tr key={i}>
                   <Th borderRight="1px solid var(--chakra-colors-blackAlpha-100)">
@@ -52,28 +52,6 @@ function AdditionTable({ data }) {
       </TableContainer>
     </Layout>
   );
-}
-
-// This gets called on every request
-export async function getServerSideProps() {
-  let API_PATH = `https://apac-tech-summit-express-backend-default.layer0-limelight.link/calculate`;
-
-  // A lot of API calls really fast - BAD when those are not cached
-  const res = await Promise.all(
-    await matrix.map(
-      async (n) =>
-        await Promise.all(
-          n.map((m) =>
-            fetch(`${API_PATH}?operation=add&x=${m[0]}&y=${m[1]}`).then((res) =>
-              res.json()
-            )
-          )
-        )
-    )
-  );
-
-  // Pass data to the page via props
-  return { props: { data: res } };
 }
 
 export default AdditionTable;
--- routes.js
+++ routes.js
@@ -17,43 +17,7 @@ export default new Router()
   .match("/service-worker.js", ({ serviceWorker }) => {
     return serviceWorker(".next/static/service-worker.js");
   })
-  .match("/api/:path*", ({ proxy, cache, removeUpstreamResponseHeader }) => {
+  .match("/api/:path*", ({ proxy }) => {
     proxy("api", { path: "/:path" });
-    removeUpstreamResponseHeader("cache-control");
-    cache({
-      browser: {
-        maxAgeSeconds: 0,
-        serviceWorkerSeconds: ONE_HOUR,
-      },
-      edge: {
-        maxAgeSeconds: ONE_MINUTE,
-        staleWhileRevalidateSeconds: ONE_DAY,
-      },
-    });
-  })
-  .get("/tables/:table", ({ cache }) => {
-    cache({
-      browser: {
-        maxAgeSeconds: 0,
-        serviceWorkerSeconds: ONE_HOUR,
-      },
-      edge: {
-        maxAgeSeconds: ONE_DAY,
-        staleWhileRevalidateSeconds: ONE_HOUR,
-      },
-    });
-  })
-  // Products - getServerSideProps
-  .get("/_next/data/:version/tables/:table.json", ({ cache }) => {
-    cache({
-      browser: {
-        maxAgeSeconds: 0,
-        serviceWorkerSeconds: ONE_DAY,
-      },
-      edge: {
-        maxAgeSeconds: ONE_DAY,
-        staleWhileRevalidateSeconds: ONE_HOUR,
-      },
-    });
   })
   .use(nextRoutes); // automatically adds routes for all files under /pages
